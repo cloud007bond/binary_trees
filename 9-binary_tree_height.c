b56c14491c4e8eff919f5a7b528e9aa6575331d5
HibeeK <akintolauser@gmail.com>
2022-12-15 22:30:15 +0000

    fixed 9-binary

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -3,7 +3,6 @@
 /**
  * count_tree_left - counts the number of levels in the tree
  * @tree: the pointer to the node
- *
  * Return: number of levels
  */
 size_t count_tree_left(const binary_tree_t *tree)
@@ -13,11 +12,9 @@ size_t count_tree_left(const binary_tree_t *tree)
 
 	return (1 + count_tree_left(tree->left));
 }
-
 /**
  * count_tree_right - counts the number of levels in the tree
  * @tree: the pointer to the node
- *
  * Return: number of levels
  */
 size_t count_tree_right(const binary_tree_t *tree)
@@ -26,12 +23,9 @@ size_t count_tree_right(const binary_tree_t *tree)
 		return (-1);
 	return (1 + count_tree_right(tree->right));
 }
-
-
 /**
  * binary_tree_height - A function that measures the height of a binary tree
  * @tree: a pointer to the root node of the tree to measure the height
- *
  * Return: if tree is null return 0, else return the height
  */
 size_t binary_tree_height(const binary_tree_t *tree)
@@ -47,6 +41,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (number_one > number_two)
 		return (number_one);
-
+nser
 	return (number_two);
 }
